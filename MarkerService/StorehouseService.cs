b2a1a54f8214587a98dd654f4bbf433b4e50cbe0
getNick <aleksandr.kostiuk687@gmail.com>
2018-05-12 20:58:23 +0300

    Base project structure

---
+++ MarkerService/StorehouseService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MarketCore.Classes;
+
+namespace MarkerService
+{
+    class StorehouseService
+    {
+        private List<Storehouse> Storehouses { get;}
+
+        public StorehouseService()
+        {
+            Storehouses=new List<Storehouse>();
+        }
+        public void AddStorehouse(Storehouse storehouse)
+        {
+            if(storehouse==null) throw new ArgumentNullException(nameof(storehouse));
+            Storehouses.Add(storehouse);
+        }
+
+        public void CloseStorehouse(Storehouse storehouse)
+        {
+            if(storehouse==null)throw new ArgumentNullException(nameof(storehouse));
+            storehouse.Products.Clear();
+            Storehouses.Remove(storehouse);
+
+        }
+        public void AddProduct(Storehouse storehouse,ProductVariation productVariation, int count)
+        {
+            if (storehouse.Products.ContainsKey(productVariation))
+            {
+                storehouse.Products[productVariation] += count;
+            }
+            else
+            {
+                storehouse.Products.Add(productVariation,count);
+            }
+        }
+
+        public List<Product> GetProduct(Product product, int count)
+        {
+            if (!Products.ContainsKey(product)) return null;
+            if (Products[product] <= 0) return null;
+            if (Products[product] < count)
+            {
+
+            }
+            return null;
+        }
+    }
+}
